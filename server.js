91c29ac659bad63dc8d12ca543368830de6dfba9
metaversepeace <jamesrbroucek@gmail.com>
2022-01-19 15:18:17 -0800

    sending more index and show data

--- server.js
+++ server.js
@@ -12,6 +12,12 @@ require('dotenv').config()
 const PORT = process.env.PORT
 const app = express()
 
+// MIDDLEWARE
+app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+
 // ROUTES
 app.get('/', (req, res) => {
   res.send('Welcome to an Awesome App about Breads!')
@@ -20,6 +26,11 @@ app.get('/', (req, res) => {
 // Breads
   const breadsController = require('./controllers/breads_controller.js')
   app.use('/breads', breadsController)
+
+// 404 Page
+app.get('*', (req, res) => {
+    res.send('404')
+  })
 
 // LISTEN
 app.listen(PORT, () => {
